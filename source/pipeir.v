b3164968196b7f497376585f771e747a058d0bd1
Ivanyinfan <1315328976@qq.com>
2018-01-13 15:53:32 +0800

    【删除】删除不必要的文件

--- source/pipeir.v
+++
@@ -1,7 +0,0 @@
-module pipeir ( pc4,ins,wpcir,clock,resetn,dpc4,inst,dbubble);
-	input [31:0] pc4,ins;
-	input wpcir,clock,resetn,dbubble;
-	output [31:0] dpc4,inst;
-	DffER32 savepc(pc4,clock,resetn,~wpcir,dbubble,dpc4);
-	DffER32 saveinst(ins,clock,resetn,~wpcir,dbubble,inst);
-endmodule
--- source/pipepc.v
+++
@@ -1,7 +0,0 @@
-module pipepc ( npc,wpcir,clock,resetn,pc );
-//程序计数器模块，是最前面一级IF流水段的输入。
-	input [31:0] npc;
-	input wpcir,clock,resetn;
-	output [31:0] pc;
-	dffe32 ip (npc,clock,resetn,wpcir,pc);  // define a D-register for PC
-endmodule
