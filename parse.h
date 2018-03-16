4e5b0dff81f4d27b7d2be3935397de47ea3333bf
Ivanyinfan <1315328976@qq.com>
2018-03-16 08:15:00 -0700

    [INITIALIZE]OS lab1 initial file

--- parse.h
+++
@@ -1,8 +0,0 @@
-/* function prototype from parse.c */
-
-#ifndef PARSE_H
-#define PARSR_H
-
-A_exp parse(string fname);
-
-#endif
--- prabsyn.h
+++
@@ -1,9 +0,0 @@
-/* function prototype from prabsyn.c */
-
-#ifndef PRABSYN_H
-#define PRABSYN_H
-
-void pr_exp(FILE *out, A_exp v, int d);
-
-#endif
-
