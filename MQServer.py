e667099e1a69aeaff4a3428ba393580b20ed1ede
Ivanyinfan <1315328976@qq.com>
2019-05-31 23:30:49 +0800

    整合类；支持动态传参；发布端唯一；采用topic路由

--- MQServer.py
+++ MQServer.py
@@ -3,21 +3,21 @@ import threading
 
 SUBTOPUBQUE = 'SUBSCRIBE'
 CORRELATION_ID = str(20190329)
+EXCHANGE_NAME = 'TEST'
 
 
-class Sender():
-    def __init__(self, pikaPara):
-        pikaConPara = pika.ConnectionParameters(**pikaPara)
-        exchangePara = {'exchange': 'test', 'exchange_type': 'topic'}
-        self.connection = pika.BlockingConnection(pikaConPara)
-        self.channel = self.connection.channel()
-        self.exchange = self.channel.exchange_declare(**exchangePara)
-        self.properties = pika.BasicProperties(delivery_mode=2)
+class Server():
+    def __init__(self):
+        self.connection = None
+        self.channel = None
+        self.exchange = None
+        self.properties = pika.BasicProperties(
+            delivery_mode=2, correlation_id=CORRELATION_ID)
 
     def send(self, routing_key, data):
-        print('[_Sender_send]routing_key=%s' % (routing_key))
+        # print('[Server][send]routing_key=%s' % (routing_key))
         pubPara = {
-            'exchange': 'test',
+            'exchange': EXCHANGE_NAME,
             'routing_key': routing_key,
             'properties': self.properties,
             'body': data
@@ -25,62 +25,81 @@ class Sender():
         self.channel.basic_publish(**pubPara)
 
     def startConsuming(self):
-        print('[_Sender_startConsuming]...')
+        print('[Server][startConsuming]...')
         self.channel.start_consuming()
+
+    def stopConsuming(self):
+        self.channel.stop_consuming()
 
     def judgeCorID(self, id):
         return id == CORRELATION_ID
 
 
+class Sender(Server):
+    def __init__(self, host, port):
+        Server.__init__(self)
+        pikaConPara = pika.ConnectionParameters(host=host, port=port)
+        exchangePara = {'exchange': EXCHANGE_NAME, 'exchange_type': 'topic'}
+        self.connection = pika.BlockingConnection(pikaConPara)
+        self.channel = self.connection.channel()
+        self.exchange = self.channel.exchange_declare(**exchangePara)
+
+
 class SenderSub(Sender):
-    def __init__(self, pikaPara, callback):
-        Sender.__init__(self, pikaPara)
-        self.subscribe = self.channel.queue_declare(SUBTOPUBQUE)
+    def __init__(self, host, port, callback):
+        Sender.__init__(self, host, port)
+        self.subscribe = self.channel.queue_declare(
+            SUBTOPUBQUE, exclusive=True)
         self.channel.basic_consume(
             queue=self.subscribe.method.queue, no_ack=True, consumer_callback=callback)
 
     def listenSub(self):
-        print('[_Sender_listenSub]...')
-        self.channel.start_consuming()
+        Sender.startConsuming(self)
 
 
-class Receiver():
-    def __init__(self, pikaPara, callback):
-        pikaConPara = pika.ConnectionParameters(**pikaPara)
+class Receiver(Server):
+    def __init__(self, name, host, port, callback=None):
+        Server.__init__(self)
+        pikaConPara = pika.ConnectionParameters(host=host, port=port)
         self.connection = pika.BlockingConnection(pikaConPara)
         self.channel = self.connection.channel()
-        self.exchangeName = 'test'
-        exchangePara = {'exchange': self.exchangeName,
-                        'exchange_type': 'topic'}
+        exchangePara = {'exchange': EXCHANGE_NAME, 'exchange_type': 'topic'}
         self.exchange = self.channel.exchange_declare(**exchangePara)
-        self.queue = self.channel.queue_declare(exclusive=True)
+        self.queue = self.channel.queue_declare(name, exclusive=True)
         self.queueName = self.queue.method.queue
+        if callback == None:
+            callback = self._call_back
         self.channel.basic_consume(callback, queue=self.queueName, no_ack=True)
-        self.properties = pika.BasicProperties(
-            delivery_mode=2, correlation_id=CORRELATION_ID)
 
     def subscibe(self, tableName):
-        print('[_Receiver_subscibe]tableName='+tableName)
-        self.__sendSub(tableName)
-        self.__receive(tableName)
+        print('[Receiver][subscibe]tableName='+tableName)
+        self._sendSub('addTable', tableName)
 
-    def stopConsuming(self):
-        self.channel.stop_consuming()
+    def unSubscibe(self, tableName):
+        print('[Receiver][unSubscibe]tableName='+tableName)
+        self._sendSub('deleteTable', tableName)
 
-    def __sendSub(self, tableName):
+    def _sendSub(self, operation, tableName=None):
+        body = {
+            'reply': self.queueName,
+            'operation': operation,
+            'tableName': tableName
+        }
         pubPara = {
             'exchange': '',
             'routing_key': SUBTOPUBQUE,
             'properties': self.properties,
-            'body': tableName
+            'body': str(body)
         }
         self.channel.basic_publish(**pubPara)
 
-    def __receive(self, tableName):
-        print('[_Receiver__receive]tableName='+tableName)
-        self.channel.queue_bind(exchange=self.exchangeName,
-                                queue=self.queueName, routing_key=tableName)
+    def receive(self):
+        print('[Receiver][receive]...')
+        routing_key = [self.queueName+'.*', 'ALL.*']
+        for rk in routing_key:
+            self.channel.queue_bind(exchange=EXCHANGE_NAME,
+                                queue=self.queueName, routing_key=rk)
         self.channel.start_consuming()
 
-    def __callback(self, channel, method, properties, body):
-        print(" [x] %r:%r" % (method.routing_key, body))
+    def _call_back(self, channel, method, properties, body):
+        pass
