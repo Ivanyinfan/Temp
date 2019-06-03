f7804a888bee893ca5ab2dadf63a0fb017862935
Ivanyinfan <1315328976@qq.com>
2019-06-03 22:23:32 +0800

    新增检查发送端存在功能；将类变量设为保护类型

--- MQServer.py
+++ MQServer.py
@@ -7,11 +7,12 @@ EXCHANGE_NAME = 'TEST'
 
 
 class Server():
-    def __init__(self):
-        self.connection = None
-        self.channel = None
-        self.exchange = None
-        self.properties = pika.BasicProperties(
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._connect()
+        self._exchange = None
+        self._properties = pika.BasicProperties(
             delivery_mode=2, correlation_id=CORRELATION_ID)
 
     def send(self, routing_key, data):
@@ -19,40 +20,44 @@ class Server():
         pubPara = {
             'exchange': EXCHANGE_NAME,
             'routing_key': routing_key,
-            'properties': self.properties,
+            'properties': self._properties,
             'body': data
         }
-        self.channel.basic_publish(**pubPara)
+        self._channel.basic_publish(**pubPara)
 
     def startConsuming(self):
         print('[Server][startConsuming]...')
-        self.channel.start_consuming()
+        self._channel.start_consuming()
 
     def stopConsuming(self):
-        self.channel.stop_consuming()
+        self._channel.stop_consuming()
 
     def judgeCorID(self, id):
         return id == CORRELATION_ID
 
     def sendAck(self, delivery_tag):
-        self.channel.basic_ack(delivery_tag=delivery_tag)
+        self._channel.basic_ack(delivery_tag=delivery_tag)
+
+    def _connect(self):
+        pikaConPara = pika.ConnectionParameters(
+            host=self._host, port=self._port)
+        self._connection = pika.BlockingConnection(pikaConPara)
+        self._channel = self._connection.channel()
+
 
 class Sender(Server):
     def __init__(self, host, port):
-        Server.__init__(self)
-        pikaConPara = pika.ConnectionParameters(host=host, port=port)
+        Server.__init__(self, host, port)
         exchangePara = {'exchange': EXCHANGE_NAME, 'exchange_type': 'topic'}
-        self.connection = pika.BlockingConnection(pikaConPara)
-        self.channel = self.connection.channel()
-        self.exchange = self.channel.exchange_declare(**exchangePara)
+        self._exchange = self._channel.exchange_declare(**exchangePara)
 
 
 class SenderSub(Sender):
     def __init__(self, host, port, callback):
         Sender.__init__(self, host, port)
-        self.subscribe = self.channel.queue_declare(
+        self.subscribe = self._channel.queue_declare(
             SUBTOPUBQUE, exclusive=True)
-        self.channel.basic_consume(
+        self._channel.basic_consume(
             queue=self.subscribe.method.queue, no_ack=True, consumer_callback=callback)
 
     def listenSub(self):
@@ -61,23 +66,33 @@ class SenderSub(Sender):
 
 class Receiver(Server):
     def __init__(self, name, host, port, callback=None):
-        Server.__init__(self)
-        pikaConPara = pika.ConnectionParameters(host=host, port=port)
-        self.connection = pika.BlockingConnection(pikaConPara)
-        self.channel = self.connection.channel()
+        Server.__init__(self, host, port)
+        self._checkSender()
         exchangePara = {'exchange': EXCHANGE_NAME, 'exchange_type': 'topic'}
-        self.exchange = self.channel.exchange_declare(**exchangePara)
+        self._exchange = self._channel.exchange_declare(**exchangePara)
         queuePara = {
             'queue': name,
             'exclusive': True,
             'durable': True,
             'auto_delete': True,
         }
-        self.queue = self.channel.queue_declare(**queuePara)
+        self.queue = self._channel.queue_declare(**queuePara)
         self.queueName = self.queue.method.queue
         if callback == None:
             callback = self._call_back
-        self.channel.basic_consume(callback, queue=self.queueName, no_ack=False)
+        self._channel.basic_consume(
+            callback, queue=self.queueName, no_ack=False)
+
+    def _checkSender(self):
+        try:
+            self._channel.queue_declare(SUBTOPUBQUE, exclusive=True)
+        except pika.exceptions.ChannelClosed:
+            self._connect()
+            return True
+        else:
+            print('[Receiver]WARNING: publisher not exists')
+            self._channel.queue_delete(SUBTOPUBQUE)
+            return False
 
     def subscibe(self, tableName):
         print('[Receiver][subscibe]tableName='+tableName)
@@ -96,18 +111,18 @@ class Receiver(Server):
         pubPara = {
             'exchange': '',
             'routing_key': SUBTOPUBQUE,
-            'properties': self.properties,
+            'properties': self._properties,
             'body': str(body)
         }
-        self.channel.basic_publish(**pubPara)
+        self._channel.basic_publish(**pubPara)
 
     def receive(self):
         print('[Receiver][receive]...')
         routing_key = [self.queueName+'.*', 'ALL.*']
         for rk in routing_key:
-            self.channel.queue_bind(exchange=EXCHANGE_NAME,
-                                    queue=self.queueName, routing_key=rk)
-        self.channel.start_consuming()
+            self._channel.queue_bind(exchange=EXCHANGE_NAME,
+                                     queue=self.queueName, routing_key=rk)
+        self._channel.start_consuming()
 
     def _call_back(self, channel, method, properties, body):
         pass
